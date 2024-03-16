8d41d08180e574a2649597f23971dc92bb66965a
Sophie Boyer <scboyer.dev@gmail.com>
2024-03-16 16:21:24 +0100

    conceptPage

--- pages/ConceptPage.jsx
+++ pages/ConceptPage.jsx
@@ -1,7 +1,104 @@
 export default function ConceptPage() {
   return (
     <main>
-      Page en cours de création. C'est pour bientôt ! 🫡
+      <div>
+        <h3>Entr'Auteurs, c'est quoi ?</h3>
+        <div>
+          Une communauté d'auteurs francophones qui publient leurs histoires sur
+          Wattpad.
+        </div>
+      </div>
+      <br />
+      <div>
+        <h3>Quel est le but de cette communauté ?</h3>
+        <div>
+          Partager sa passion pour l'écriture, faire découvrir son texte à
+          d'autres, obtenir des avis constructifs pour s'améliorer, participer à
+          des ateliers, échanger, se motiver...
+        </div>
+      </div>
+      <br />
+      <div>
+        <h3>Quelles sont les conditions pour rejoindre cette communauté ?</h3>
+        <div>
+          Nous rejoindre sur le serveur Discord.
+          <br />
+          Avoir un compte sur Wattpad.
+        </div>
+      </div>
+      <br />
+      <div>
+        <h3>Comment participer aux échanges d'avis ou au concours ?</h3>
+        <div>
+          Une fois que ton compte est validé, tu peux ajouter une histoire.
+          Ensuite, tu peux l'inscrire au concours ou à l'échange.
+          <br />
+          Attention, seuls les auteurs majeurs peuvent s'inscrire au concours
+          car ils sont susceptibles de tomber sur des histoires avec du contenu
+          mature. Mais les auteurs mineurs peuvent quand même participer aux
+          échanges.
+        </div>
+      </div>
+      <br />
+      <div>
+        <h3>C'est quoi, les échanges d'avis ?</h3>
+        <div>
+          Au 1er de chaque mois, un tirage est réalisé. Chaque participant
+          reçoit une histoire au hasard. Il doit y consacrer 2 heures de
+          lecture, puis remplir la fiche avis en mettant une note sur 2 et un
+          commentaire pour chacun des 5 critères :
+          <br />
+          <ul>
+            <li>
+              <strong>Orthographe :</strong> s'il n'y a aucune faute ou très peu
+              (2/2), environ 1 ou 2 par paragraphe (1/2) ou beaucoup (0/2){" "}
+            </li>
+            <li>
+              <strong>Style :</strong> si le style est fluide, avec un
+              vocabulaire riche (note de 0 à 2/2)
+            </li>
+            <li>
+              <strong>Cohérence :</strong> est-ce que l'histoire est logique ?
+              le développement des personnages réaliste ?... (note de 0 à 2/2)
+            </li>
+            <li>
+              <strong>Suspens :</strong> est-ce qu'à la fin de ces deux heures
+              de lecture, tu voudrais continuer votre lecture pour apprendre ce
+              qui se passe après ? Est-ce que tu as été surpris(e) par certains
+              rebondissements ? (note de 0 à 2/2)
+            </li>
+            <li>
+              <strong>Dialogues :</strong> est-ce que la syntaxe est respectée
+              (tirets ou guillemets, incises) ? est-ce que la façon de parler
+              des perosnnages est cohérente avec l'époque et leur personnalité ?
+              (note de 0 à 2/2)
+            </li>
+          </ul>
+        </div>
+      </div>
+      <br />
+      <div>
+        <h3>
+          Que se passe-t-il si je ne suis pas d'accord avec l'avis laissé ?
+        </h3>
+        <div>
+          Tu peux contester un avis, seulement si tu estimes qu'il manque de
+          bienveillance, qu'il est inappropprié, injustifié... <br />
+          Dans ce cas, un administrateur traitera ta contestation. S'il juge que
+          l'avis est en effet déplacé, il l'annulera et le participant qui
+          l'aura posté aura un avertissement.
+          <br />
+          Attention à ne pas abuser des contestations sans raison, car si
+          l'administrateur juge que l'avis était pertinent et bienveillant,
+          c'est toi qui aura un avertissement.
+        </div>
+      </div>
+      <br />
+      <div>
+        <h3>Les avertissements, ça fait quoi ?</h3>
+        <div>Au bout de 3 avertissements, ton compte est suspendu.</div>
+      </div>
+
       <br />
     </main>
   );
